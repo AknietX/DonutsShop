81cebca3ce9a8b64aa1b335b3d9d8c754a1523dc
AknietX <kvarplat2@gmail.com>
2025-04-27 15:26:34 +0600

    без коментариев

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -3,13 +3,12 @@ import FetchSup from '../components/Fetch-Sup.vue';
 import MainFooter from '../components/MainFooter.vue';
 import HeaderEtc from '@/components/Header-Etc.vue';
 import MainHeader from '../components/MainHeader.vue';
-import Catalog from '@/components/Catalog.vue';
+
 </script>
 
 <template>
   <MainHeader />
   <HeaderEtc />
-  <Catalog />
   <FetchSup />
   <MainFooter />
 </template>
